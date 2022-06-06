0ad87803be36990b74e0b4c6c5db0c2962da2914
melakualehgn <melakualehegn34@gmail.com>
2022-06-06 15:07:18 -0700

    Find the max fixed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxu = my_list[0]
-    for i in my_list:
-        if i > maxu:
-            maxu = i
+    if my_list:
+        maxu = my_list[0]
+        for i in my_list:
+            if i > maxu:
+                maxu = i
     return maxu
