8754e1488e7db3cf4455420745ac745182e1aff5
melakualehgn <melakualehegn34@gmail.com>
2022-06-20 14:12:46 -0700

    pycodestyle for task-0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     c = 0
-    try: 
+    try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             c += 1
