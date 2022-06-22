705ffbb0f30b52ea1118f575f5f3eaf84a9ee6fd
melakualehgn <melakualehegn34@gmail.com>
2022-06-22 03:22:16 -0700

    pycodestyle checked

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''empty class'''
+
+
 class Square:
     '''an empty class to define a square'''
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''empty class'''
+
+
+class Square:
+    '''a square class with private instance attribute'''
+
+    def __init__(self, size):
+        '''initialize size of square'''
+
+        self.size = size
