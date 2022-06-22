ada871d541c3da7248f3ac22b823540adba8fe50
melakualehgn <melakualehegn34@gmail.com>
2022-06-22 04:15:15 -0700

    Docstyle fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,12 @@ class Square:
     '''a square class with private instance attribute'''
 
     def __init__(self, size=0):
-        '''initialize size of square'''
+        '''initialize size of square
+        Args:
+            size (int): size of side of square
+        Returns:
+            None
+        '''
 
         self.__size = size
         if type(size) is not int:
