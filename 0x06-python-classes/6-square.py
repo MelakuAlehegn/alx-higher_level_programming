2cd5a70d448c67445c1745200592c29800675abe
melakualehgn <melakualehegn34@gmail.com>
2022-06-23 12:17:23 -0700

    task-6 fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,70 +1,72 @@
-#!/usr/bin/pythoan3
-"""
-a class that defines a Square
-"""
+#!/usr/bin/python3
+"""a square class"""
 
 
-class Square:
-    """
-    A class that defines a Square
-    """
+class Square():
+    """a class constructor"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Instantiation function"""
+        """private attribute"""
+
         self.__size = size
-        self.__position = position
+        self.position = position
 
     def area(self):
-        """
-        A public instance method
-        Args:
-        Returns:
-            the area of the square
-        """
-        return (self.__size ** 2)
+        """public method to calculates area"""
+
+        return self.__size ** 2
 
     @property
     def size(self):
-        """
-        getter function for size
-        """
+        """getter method property size"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is int:
-            if value >= 0:
-                self.__size = value
-            else:
-                raise ValueError("size must be >= 0")
-        else:
+        """setter method validate value"""
+
+        if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     @property
     def position(self):
-        """
-        Getter function for position
-        """
+        """position getter method property decorator"""
+
         return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is tuple and len(value) is 2 \
-                and type(value[0]) is int and type(value[1]) is int \
-                and value[0] >= 0 and value[0] >= 0:
-            self.__position = value
-        else:
+        """setter method validate value"""
+
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
-        """
-        prints the square with the #
-        """
-        if self.__size > 0:
-            for i in range(self.__position[1]):
-                print()
-            for x in range(self.__size):
-                print(' ' * self.__position[0], end='')
-                print("#" * self.__size)
+        """prints a # square"""
+
+        num = self.size
+        line = self.position
+
+        if num is 0:
+            print("")
         else:
-            print()
+            for i in range(line[1]):
+                print("")
+            for j in range(num):
+                for n in range(num + line[0]):
+                    if n < line[0]:
+                        print(" ", end='')
+                    else:
+                        print("#", end='')
+                print("")
