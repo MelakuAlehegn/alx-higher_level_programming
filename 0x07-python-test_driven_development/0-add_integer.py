6d7e749be3833434ebee20e82dcca75dac2adb7c
melakualehgn <melakualehegn34@gmail.com>
2022-06-27 06:43:58 -0700

    documentation edited

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
-
 '''
    0-add_integer function
    a function that adds two integers
+   raises TypeError for non ints
 '''
 
 
 def add_integer(a, b=98):
     '''
-    a function to return sum of two integers
+    a function to return sum of two integers, converts floats to int
     '''
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
