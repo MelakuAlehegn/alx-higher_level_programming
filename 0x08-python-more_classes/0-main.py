1337b0c1e3007d5ee233b72c83a14a39be4ebdf6
melakualehegn <melakualehegn34@gmail.com>
2022-06-28 22:33:21 +0300

    everything is an object fix

--- 0x08-python-more_classes/0-main.py
+++ 0x08-python-more_classes/0-main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
-
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+l1 = [1, 2, 3]
+l2 = l1
+l1.append(4)
+print(l2)
