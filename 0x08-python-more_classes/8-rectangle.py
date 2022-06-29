2894eb21e0d55209f8d9d7deee450c308965d5fa
melakualehegn <melakualehegn34@gmail.com>
2022-06-29 16:26:30 +0300

    task-8 fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -90,9 +90,9 @@ class Rectangle:
         static method that returns the biggest recrtangle
         raises TypeError is args are not instances
         '''
-        if type(rect_1) is not Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TimeoutError('rect_1 must be an instance of Rectangle')
-        if type(rect_2) is not Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TimeoutError('rect_2 must be an instance of Rectangle')
         a1 = rect_1.area()
         a2 = rect_2.area()
