1c693d61c3b3bbd410c96ab963cb302621656ca1
melakualehegn <melakualehegn34@gmail.com>
2022-07-04 23:59:43 +0300

    task-8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+a module for rectange
+'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Reactangele:
+    def __init__(self, width, height):
+        '''private instantiation of attributes
+           validate type with integer_validator
+        '''
+        super().integer_validator('width', width)
+        self.__width = width
+        super().integer_validator('height', height)
+        self.__height = height
