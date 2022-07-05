ede8916a7fc25e8ba71b29f454d81008ffb2af21
melakualehegn <melakualehegn34@gmail.com>
2022-07-06 00:26:22 +0300

    task-0 fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,12 +2,14 @@
 '''
 read a text file
 '''
+
+
 def read_file(filename=""):
     '''
     reads file named filename
     Args:
         - filename: name of file
     '''
-    with open (filename) as f:
+    with open(filename) as f:
         line = f.read()
         print(line, end='')
