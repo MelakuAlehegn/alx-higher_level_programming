afd078e62db4753c23c666b827ad13baac224b65
melakualehegn <melakualehegn34@gmail.com>
2022-07-06 00:47:50 +0300

    task-1 fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,11 @@ writes a string to a textfile
 def write_file(filename="", text=""):
     '''
     writes text to filename
+    Args:
+        -filename: file to write to
+        -text: text to write
+    Returns: number of chars written
     '''
-    with open('filename', 'w') as f:
+    with open(filename, 'w') as f:
         writes = f.write(text)
         return writes
