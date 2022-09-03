24d2115f426e3485204b94c1b29a0e5586c5e421
melakualehegn <melakualehegn34@gmail.com>
2022-09-03 22:41:06 +0300

    TASK-7:FIX

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,6 +8,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
     (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    state_id INT FOREIGN KEY REFERENCES states(id),
-    name VARCHAR(256)
+    FOREIGN KEY(state_id) REFERENCES states(id),
+    name VARCHAR(256) NOT NULL
     );
