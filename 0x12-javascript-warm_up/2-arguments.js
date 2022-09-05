13a4bc160b06bd4837dd3e86674effea9a064a43
melakualehegn <melakualehegn34@gmail.com>
2022-09-05 23:50:40 +0300

    Task-2:fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else {
+if (process.argv.length >= 3) {
   console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
