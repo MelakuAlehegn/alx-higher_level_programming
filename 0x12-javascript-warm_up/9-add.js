093098214103c58de410c4e64def7ac54d12b439
melakualehegn <melakualehegn34@gmail.com>
2022-09-06 01:32:38 +0300

    task-9:fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add(a,b) {
-  const sum = a + b
-  console.log(sum)  
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
 }
 const c1 = parseInt(process.argv[2]);
 const c2 = parseInt(process.argv[3]);
