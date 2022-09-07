43f3a2298752e05734c13ab594383cbeab1e0757
melakualehegn <melakualehegn34@gmail.com>
2022-09-07 18:20:43 +0300

    Task-8:semistandard fixed

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-exports.esrever = function (list){
-    let newList = [];
-    for (let a = list.length - 1; a >= 0; a--) {
-        newList.push(list[a]);
-    }
-    return newList;
-}
+exports.esrever = function (list) {
+  const newList = [];
+  for (let a = list.length - 1; a >= 0; a--) {
+    newList.push(list[a]);
+  }
+  return newList;
+};
