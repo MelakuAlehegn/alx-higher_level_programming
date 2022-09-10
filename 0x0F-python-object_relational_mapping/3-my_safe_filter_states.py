0747a6d3693ee64235d49fc6ec81083785cd37d1
melakualehegn <melakualehegn34@gmail.com>
2022-09-10 21:27:05 +0300

    Task-3:SQL Injection... fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
                              passwd=sys.argv[2], db=sys.argv[3],
                              charset="utf8")
     curr = db_con.cursor()
-    curr.execute("SELECT * FROM states WHERE name LIKE %s \
-    ORDER BY id ASC", (sys.argv[4],))
+    curr.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                 (sys.argv[4],))
     states = curr.fetchall()
     for state in states:
         if state[1] == sys.argv[4]:
