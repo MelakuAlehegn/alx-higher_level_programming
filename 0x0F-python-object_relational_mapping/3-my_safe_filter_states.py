b30f5631ecbc128ce7e9def156039798116bd42b
melakualehegn <melakualehegn34@gmail.com>
2022-09-10 21:23:02 +0300

    Task-3:SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''a script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3],
+                             charset="utf8")
+    curr = db_con.cursor()
+    curr.execute("SELECT * FROM states WHERE name LIKE '%s' \
+    ORDER BY id ASC", (sys.argv[4],))
+    states = curr.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    curr.close()
+    db_con.close()
