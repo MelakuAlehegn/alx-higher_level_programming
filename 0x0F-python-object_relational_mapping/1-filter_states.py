aa7319f34eb01e05199c77625a53abab0476854e
melakualehegn <melakualehegn34@gmail.com>
2022-09-10 13:14:46 +0300

    Task-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if states[1][0] == 'N':
+            print(state)
     cur.close()
     db_con.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3],
+                             charset="utf8")
+    curr = db_con.cursor()
+    curr.execute("SELECT * FROM states WHERE name == %s",(sys.argv[4]))
+    states = curr.fetchall()
+    for state in states:
+        print(state)
+    curr.close()
+    db_con.close()
