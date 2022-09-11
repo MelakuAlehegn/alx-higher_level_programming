070353284498f4cfc4c897b45b11341d7574886b
melakualehegn <melakualehegn34@gmail.com>
2022-09-12 00:39:56 +0300

    Task-14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
+    cities = session.query(City, State).filter(City.state_id == State.id)\
                                      .order_by(City.id).all()
-    for city, state in rows:
+    for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
