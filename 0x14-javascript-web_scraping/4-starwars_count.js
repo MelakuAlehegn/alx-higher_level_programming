27f7a6e9ce598e9e340b62b406fc6dfa3095b046
melakualehegn <melakualehegn34@gmail.com>
2022-10-19 00:03:53 +0300

    Task-3:typo

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+const request = require('request');
+const id = process.argv[2];
+request(`https://swapi-api.hbtn.io/api/films/${id}`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
