62d88404fddaaa107fe894a17526eb1f7d7b69d4
melakualehegn <melakualehegn34@gmail.com>
2022-10-18 23:20:10 +0300

    Task-1:fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 // a script that display the status code of a GET request
 const request = require('request');
 const URL = process.argv[2];
-request('url', function(response){
-    console.log('code:', response.statusCode);
+request(URL, function (response) {
+  console.log('code:', response.statusCode);
 });
